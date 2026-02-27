7f825890c70d831a4503a149569b6f5aff2ea310
Kondareddy <rkonda959@gmail.com>
2026-02-27 15:04:25 +0530

    DOM

---
+++ Dom.js
@@ -0,0 +1,18 @@
+// const title_elements = document.querySelectorAll(".family");
+
+// title_elements.forEach(element => {
+//     console.log(element);
+// });
+
+
+// const allContacts = document.querySelectorAll('p')
+// console.log(allContacts)
+// allContacts.forEach(element => {
+//     console.log(element)
+// });
+
+const firstWorkContact = document.querySelectorAll('button')
+console.log(firstWorkContact)
+
+const thirdBtn = document.querySelector('div button:nth-child(3)')
+console.log(thirdBtn)
